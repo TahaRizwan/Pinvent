bdbfdffce9c8182b80cfef616a4423a7864f2e25
TahaRizwan <tahabhaiyag@gmail.com>
2022-10-30 00:23:08 +0500

    commit

--- backend/server.js
+++ backend/server.js
@@ -19,8 +19,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["https://pinvent-4b872x0xr-taharizwanmughal-gmailcom.vercel.app/", "https://pinvent-app.vercel.app"],
-    credentials: true,
+    origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
   })
 );
 
