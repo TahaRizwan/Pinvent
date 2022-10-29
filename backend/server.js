0d577d756b91033e0338f7d4a914a3b0e34abd67
TahaRizwan <tahabhaiyag@gmail.com>
2022-10-29 23:44:27 +0500

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,50 @@
-const dotenv = require('dotenv').config();
-const express = require("express")
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const userRoute = require('./routes/userRoute');
-const errorHandler = require('./middleWare/errorMiddleware');
-const cookieParser = require('cookie-parser')
+const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const userRoute = require("./routes/userRoute");
+const productRoute = require("./routes/productRoute");
+const contactRoute = require("./routes/contactRoute");
+const errorHandler = require("./middleWare/errorMiddleware");
+const cookieParser = require("cookie-parser");
+const path = require("path");
 
-const app = express()
+const app = express();
 
-//Middlewares
-app.use(express.json())
-app.use(cookieParser())
-app.use(express.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(cors())
+// Middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://pinvent-app.vercel.app"],
+    credentials: true,
+  })
+);
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Routes Middleware
-app.use('/api/users', userRoute)
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/contactus", contactRoute);
 
 // Routes
 app.get("/", (req, res) => {
-    res.send("Home Page")
-})
+  res.send("Home Page");
+});
 
 // Error Middleware
-app.use(errorHandler)
-
-const PORT = process.env.PORT || 5000
-
+app.use(errorHandler);
 // Connect to DB and start server
-mongoose.connect(process.env.MONGO_URI).then(() => {
+const PORT = process.env.PORT || 5000;
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
     app.listen(PORT, () => {
-        console.log(`Server Running on port ${PORT}`);
-    })
-})
-    .catch((err) => console.log(err));
+      console.log(`Server Running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
