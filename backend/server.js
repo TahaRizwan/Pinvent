07ff99257b823f98a4313b218279f5bf2e226dfd
TahaRizwan <tahabhaiyag@gmail.com>
2022-10-30 00:41:47 +0500

    commit

--- backend/server.js
+++ backend/server.js
@@ -17,16 +17,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(
-    cors({
-        origin: '*',
-        credentials: true,            //access-control-allow-credentials:true
-        optionSuccessStatus: 200,
-    })
-);
+app.use(cors())
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
     next();
 });
 
